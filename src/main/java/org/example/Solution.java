50f8f08654890ffe151936e43851a269153568e2
DmytroSa1997 <deanwincester1907@gmail.com>
2022-12-08 00:48:48 +0200

    Initial commit

---
+++ src/main/java/org/example/Solution.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Solution {
+    public static void main(String[] args){
+        for (int i = 1; i < 11; i++)
+            System.out.println(i + " * 5 = " + i*5);
+    }
+}
